026e05654b3fd3f53f5771b192750ae7c51fea9d
claudiocds1987 <cla8787@gmail.com>
2025-08-22 15:41:15 -0300

    correccion ancho del filtro

--- src/app/shared/models/item-card.model.ts
+++ src/app/shared/models/item-card.model.ts
@@ -2,6 +2,6 @@ export interface ItemCard {
     id: number | string;
     title: string;
     description: string;
-    resource: string; // Opcional si aplica a un recurso específico
+    resource: string; // Opcional si aplica a un recurso específico usuario restringido
     path: string;
 }
