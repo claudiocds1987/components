1ecf5ce22c2138329a222c1ca0c09cc332b3848c
claudiocds1987 <cla8787@gmail.com>
2025-08-26 17:16:11 -0300

    borrado de lineas innecesarias

--- src/app/shared/components/grid/grid-filter/grid-filter.component.ts
+++ src/app/shared/components/grid/grid-filter/grid-filter.component.ts
@@ -14,9 +14,7 @@ import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatNativeDateModule } from "@angular/material/core";
 import { MatButtonModule } from "@angular/material/button";
 import { GridFilterConfig } from "../../../models/grid-filter-config.model";
-import { DateTime } from "luxon";
 import { DateInputComponent } from "../../date-input/date-input.component";
-import { Chip } from "../../chips/chips/chips.component";
 import { SkeletonDirective } from "../../../directives/skeleton.directive";
 
 @Component({
@@ -46,123 +44,18 @@ export class GridFilterComponent {
     @Input() filterForm!: FormGroup;
     @Input() isLoading = false;
     @Output() emitFilterApplied = new EventEmitter<Record<string, unknown>>();
-    //@Output() emitFilterDescriptions = new EventEmitter<Chip[]>();
 
     applyFilter(): void {
         const filterFormValues = this.filterForm.value;
         this.emitFilterApplied.emit(filterFormValues);
-        //const filterChips = this._mapFilterValuesToChips(filterFormValues); //esto sacarlo tambien
-        //this.emitFilterDescriptions.emit(filterChips); // este sacarlo cuando se resuelva todo
     }
 
     clearFilter(): void {
         this.filterForm.reset();
         this.emitFilterApplied.emit(this.filterForm.value);
-        //this.emitFilterDescriptions.emit([]);
     }
 
     isButtonDisabled(): boolean {
         return !this.filterForm?.valid || !this.filterForm?.dirty;
-    }
-
-    // borrar esta funcion
-    private _mapFilterValuesToChips(
-        filterValues: Record<string, unknown>,
-    ): Chip[] {
-        /* filterValues es el objeto con los valores de los filtros aplicados ej:
-            {
-                birthDate: null
-                id: ""
-                name: "Maria"
-                position: ""
-                surname: "perez"
-            }
-            "key" es cada propiedad del objeto birthDate, id, name, position, surname 
-            filterValues[key]; es el valor de cada propiedad del objeto ej "Maria"
-            */
-        const chips: Chip[] = [];
-
-        Object.keys(filterValues).forEach((key: string): void => {
-            const value = filterValues[key];
-            const filterConfig = this.config.find(
-                (c): boolean => c.fieldName === key,
-            );
-
-            if (
-                !filterConfig ||
-                value === null ||
-                typeof value === "undefined" ||
-                value === ""
-            ) {
-                return;
-            }
-
-            if (
-                filterConfig.fieldType === "select" &&
-                (value === "all" || value === "Todos")
-            ) {
-                return;
-            }
-
-            let displayValue: string | number | boolean;
-            let chipType: Chip["type"] = filterConfig.fieldType;
-
-            switch (filterConfig.fieldType) {
-                case "date": {
-                    const luxonDate = DateTime.fromISO(value as string);
-                    displayValue = luxonDate.isValid
-                        ? luxonDate.toFormat("dd/MM/yyyy")
-                        : "Fecha inválida";
-                    break;
-                }
-                case "dateRange": {
-                    const dateRangeValue = value as {
-                        startDate?: string;
-                        endDate?: string;
-                    };
-                    const startLuxon = dateRangeValue.startDate
-                        ? DateTime.fromISO(dateRangeValue.startDate)
-                        : null;
-                    const endLuxon = dateRangeValue.endDate
-                        ? DateTime.fromISO(dateRangeValue.endDate)
-                        : null;
-
-                    if (startLuxon && endLuxon) {
-                        displayValue = `${startLuxon.toFormat("dd/MM/yyyy")} - ${endLuxon.toFormat("dd/MM/yyyy")}`;
-                    } else if (startLuxon) {
-                        displayValue = `Desde: ${startLuxon.toFormat("dd/MM/yyyy")}`;
-                    } else if (endLuxon) {
-                        displayValue = `Hasta: ${endLuxon.toFormat("dd/MM/yyyy")}`;
-                    } else {
-                        return;
-                    }
-                    break;
-                }
-                case "select": {
-                    const selectedItem = filterConfig.selectItems?.find(
-                        (item): boolean => item.id === value,
-                    );
-                    displayValue = selectedItem?.description || `${value}`;
-                    break;
-                }
-                case "text":
-                default:
-                    displayValue = `${value}`;
-                    chipType = filterConfig.fieldType;
-                    break;
-            }
-
-            const newChip: Chip = {
-                key: key,
-                label: `${filterConfig.label}: ${displayValue}`,
-                value: value,
-                disabled: false,
-                type: chipType,
-            };
-
-            chips.push(newChip);
-        });
-
-        return chips;
     }
 }
